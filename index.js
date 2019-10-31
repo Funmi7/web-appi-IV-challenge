a18deb1bb5d50170c3bf1183d1072c8fc0525e04
Funmi7 <talabifunmilayo@gmail.com>
2019-10-31 17:09:13 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT;
+const friends = [
+  { id: 1, name: 'Shaun' },
+  { id: 2, name: 'Megan' },
+  { id: 3, name: 'Pere' },
+]
+
+app.use(static(__dirname + '/client/build'))
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
+});
+
+app.get('/api/friends', (req, res) => {
+  res.json(friends);
+});
+
+app.listen(port, () => {
+  console.log('listening on ' + port);
+});
