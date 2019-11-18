7c3f16a0db676f590b2b07c33c7a5f94dd5b225d
Funmi7 <talabifunmilayo@gmail.com>
2019-11-18 09:00:32 +0100

    Added pg for heroku

--- knexfile.js
+++ knexfile.js
@@ -14,4 +14,19 @@ module.exports = {
     },
     seeds: { directory: './data/seeds' },
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
+    },
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: { directory: './data/seeds' },
+  },
 };
